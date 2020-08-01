3fcffd2b44be50678587f98fe462e4d4eee2496d
Alan <alrgouveia21@gmail.com>
2020-07-31 23:39:51 -0300

    finalizado conf bd

--- app/app.js
+++ app/app.js
@@ -8,4 +8,36 @@ import path from 'path';
 dotenv.config();
 
 const app = express();
-app.use;
+app.use(cors());
+app.use(express.json());
+
+//Vincula o react ao app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+const { DB_CONNECTION } = process.env;
+console.log('Iniciado conexão com o Banco MongoDB');
+mongoose.connect(
+  DB_CONNECTION,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      connectedToMongoDB = false;
+      console.error(`Erro na conexão ao MongoDB - ${err}`);
+    }
+  }
+);
+
+const { connection } = mongoose;
+
+connection.once('open', () => {
+  connectedToMongoDB = true;
+  console.log('Conectado ao MongoDB');
+
+  const APP_PORT = process.env.PORT || 3001;
+  app.listen(APP_PORT, () => {
+    console.log(`Servidor iniciado na porta ${APP_PORT}`);
+  });
+});
